348f32b6d443451947f69ae9f048c5d1568a2a61
Simxn999 <simon.johansson999@gmail.com>
2023-10-20 09:59:37 +0200

    Initialized TitleGeneratorGPT

---
+++ src/Ume-Chat-Utilities/TitleGeneratorGPT/TitleGenerator.cs
@@ -0,0 +1,86 @@
+﻿using Azure;
+using Azure.AI.OpenAI;
+using Ume_Chat_Utilities;
+
+namespace TitleGeneratorGPT;
+
+/// <summary>
+///     Title Generator utilizing ChatGPT.
+/// </summary>
+public static class TitleGenerator
+{
+    /// <summary>
+    ///     Client handling Azure OpenAI.
+    /// </summary>
+    private static OpenAIClient? Client { get; set; }
+    
+    /// <summary>
+    ///     Azure OpenAI GPT Deployment name.
+    /// </summary>
+    private static string? GPTDeployment { get; set; }
+    
+    /// <summary>
+    ///     System message, title generation instructions.
+    /// </summary>
+    private static string? RoleInformation { get; set; }
+    
+    /// <summary>
+    ///     If Title Generator is initialized or not.
+    /// </summary>
+    private static bool IsInitialized { get; set; }
+    
+    /// <summary>
+    ///     Initialize Title Generator.
+    /// </summary>
+    public static void Initialize()
+    {
+        var endpoint = new Uri(Variables.Get("OPENAI_URL"));
+        var keyCredential = new AzureKeyCredential(Variables.Get("OPENAI_API_KEY"));
+        Client = new OpenAIClient(endpoint, keyCredential);
+        GPTDeployment = Variables.Get("OPENAI_GPT_DEPLOYMENT");
+        RoleInformation = Variables.Get("OPENAI_TITLE_GENERATOR_ROLEINFORMATION");
+
+        IsInitialized = true;
+    }
+    
+    /// <summary>
+    ///     Generate Title based on given content.
+    /// </summary>
+    /// <param name="content">Content to base title on</param>
+    /// <returns>String with new generated title</returns>
+    public static async Task<string> GenerateTitleAsync(string content)
+    {
+        if (!IsInitialized)
+            Initialize();
+
+        var messages = GetMessages(content);
+        return await GetChatResponse(messages);
+    }
+
+    /// <summary>
+    ///     Retrieve the messages required for chat request.
+    /// </summary>
+    /// <param name="content">Content to base title on</param>
+    /// <returns>Enumerable of ChatMessages used by GPT to generate title</returns>
+    private static IEnumerable<ChatMessage> GetMessages(string content)
+    {
+        return new[]
+        {
+            new ChatMessage(ChatRole.System, RoleInformation),
+            new ChatMessage(ChatRole.User, content)
+        };
+    }
+
+    /// <summary>
+    ///     Retrieve GPT response based on messages.
+    /// </summary>
+    /// <param name="messages">Messages to give GPT</param>
+    /// <returns>String with GPT response</returns>
+    private static async Task<string> GetChatResponse(IEnumerable<ChatMessage> messages)
+    {
+        ArgumentNullException.ThrowIfNull(Client);
+        
+        var response = await Client.GetChatCompletionsAsync(GPTDeployment, new ChatCompletionsOptions(messages));
+        return response.Value.Choices[0].Message.Content;
+    }
+}
---
+++ src/Ume-Chat-Utilities/TitleGeneratorGPT/TitleGeneratorConfigurationExtension.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.Configuration;
+
+namespace TitleGeneratorGPT;
+
+/// <summary>
+///     IConfigurationBuilder extension for adding Title Generator configurations.
+/// </summary>
+public static class TitleGeneratorConfigurationExtension
+{
+    /// <summary>
+    ///     Add Title Generator configurations to IConfigurationBuilder.
+    /// </summary>
+    /// <param name="builder">IConfigurationBuilder to add configuration on</param>
+    /// <param name="isDevelopment">If environment is development or not</param>
+    public static void AddTitleGenerator(this IConfigurationBuilder builder, bool isDevelopment)
+    {
+        var assembly = Assembly.GetAssembly(typeof(TitleGenerator));
+        var assemblyName = assembly?.GetName().Name;
+        ArgumentNullException.ThrowIfNull(assembly);
+        
+        var stream = assembly.GetManifestResourceStream($"{assemblyName}.appsettings.json");
+        ArgumentNullException.ThrowIfNull(stream);
+
+        builder.AddJsonStream(stream);
+        
+        if (!isDevelopment)
+            return;
+        
+        stream = assembly.GetManifestResourceStream($"{assemblyName}.appsettings.Development.json");
+        ArgumentNullException.ThrowIfNull(stream);
+
+        builder.AddJsonStream(stream);
+    }
+}
+
