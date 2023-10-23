1b292cc70c4cfe1570a32ae945c8814befd83ccf
Simxn999 <simon.johansson999@gmail.com>
2023-10-23 11:13:43 +0200

    Temporarily disabled model validation

--- src/Ume-Chat-API/ChatAPI/Program.cs
+++ src/Ume-Chat-API/ChatAPI/Program.cs
@@ -1,6 +1,4 @@
 using ChatAPI;
-using ChatAPI.Validation;
-using FluentValidation;
 using KeyVaultProvider;
 using Models.API.ChatAPI;
 using Utilities;
@@ -9,7 +7,6 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<IValidator<List<RequestMessage>>, RequestMessagesValidator>();
 
 builder.Configuration.AddAzureKeyVaultWithReferenceSupport();
 builder.Configuration.AddVariables();
@@ -28,7 +25,6 @@ app.MapPost("api/chat",
             async (HttpContext context, List<RequestMessage> messages, bool? stream = false) =>
             {
                 return await DataManager.ChatAsync(context, messages, stream ?? false);
-            })
-   .AddEndpointFilter<ValidationFilter<List<RequestMessage>>>();
+            });
 
 app.Run();
